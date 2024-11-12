c70676a696d5779dc962e03baf1d10e7aa5a0eee
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2024-11-12 19:26:17 +0000

    bug: chau prettier

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Wwrite your code here
+  let number = 0;
+  let cohorte = "";
+  let newCohorte = "Spain 91 Bis";
+  let myArray = [];
+  let person1 = { name: "Cristian", lastname: "Chacón" };
+  let person2 = { name: "Natalia", lastname: "Manzano" };
+  let person3 = { name: "Martin", lastname: "Martos" };
+  console.log(person2.lastname);
+  console.log(person3.name);
 };
